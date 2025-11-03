de88c8f72864ded47228845cf8a081c4e2709bb8
reinaldocs-dev <reinaldo.correia@gaia.dev>
2025-11-03 12:16:37 -0300

    refactor(select): apply data-input-container pattern
    Align Select with Input/Textarea by adding data-input-container wrapper.
    Enables contextual theming and removes 91 lines of unused CSS.

--- packages/ui/src/inputs/Select/index.tsx
+++ packages/ui/src/inputs/Select/index.tsx
@@ -32,40 +32,41 @@ const Select: React.FC<SelectProps> = ({
         </Body>
       )}
       <div className={styles.input_container_wrapper}>
-        <RadixSelect.Root
-          value={value?.toString()}
-          onValueChange={handleValueChange}
-          disabled={disabled}
-        >
-          <RadixSelect.Trigger
-            className={styles.select_trigger}
-            data-button-container
+        <div className={styles.input_container} data-input-container>
+          <RadixSelect.Root
+            value={value?.toString()}
+            onValueChange={handleValueChange}
+            disabled={disabled}
           >
-            <RadixSelect.Value
-              placeholder={placeholder || "Selecione uma opção"}
-            />
+            <RadixSelect.Trigger className={styles.select_trigger}>
+              <RadixSelect.Value
+                placeholder={placeholder || "Selecione uma opção"}
+              />
 
-            <RadixSelect.Icon className={styles.arrow}>
-              <ChevronDown size={16} />
-            </RadixSelect.Icon>
-          </RadixSelect.Trigger>
-          <RadixSelect.Portal>
-            <RadixSelect.Content className={styles.select_content}>
-              <RadixSelect.Viewport>
-                {options.map((option) => (
-                  <RadixSelect.Item
-                    key={option.value}
-                    value={option.value.toString()}
-                    disabled={option.disabled}
-                    className={styles.select_item}
-                  >
-                    <RadixSelect.ItemText>{option.label}</RadixSelect.ItemText>
-                  </RadixSelect.Item>
-                ))}
-              </RadixSelect.Viewport>
-            </RadixSelect.Content>
-          </RadixSelect.Portal>
-        </RadixSelect.Root>
+              <RadixSelect.Icon className={styles.arrow}>
+                <ChevronDown size={16} />
+              </RadixSelect.Icon>
+            </RadixSelect.Trigger>
+            <RadixSelect.Portal>
+              <RadixSelect.Content className={styles.select_content}>
+                <RadixSelect.Viewport>
+                  {options.map((option) => (
+                    <RadixSelect.Item
+                      key={option.value}
+                      value={option.value.toString()}
+                      disabled={option.disabled}
+                      className={styles.select_item}
+                    >
+                      <RadixSelect.ItemText>
+                        {option.label}
+                      </RadixSelect.ItemText>
+                    </RadixSelect.Item>
+                  ))}
+                </RadixSelect.Viewport>
+              </RadixSelect.Content>
+            </RadixSelect.Portal>
+          </RadixSelect.Root>
+        </div>
         {hint && (
           <Body variant="caption" className={styles.hint}>
             {hint}
